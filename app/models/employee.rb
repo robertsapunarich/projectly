9cf804329c44a893b8cb3221842b928e8233a057
Robert Sapunarich <robertsapunarich@gmail.com>
2024-01-24 09:02:00 -0500

    Saving work from yesterday.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
+  has_secure_password
   has_many :tasks, through: :employee_task
   enum :work_focus, [:project_manager, :development, :design, :business, :research]
 end
