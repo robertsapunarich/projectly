ec97b9bb843110d183e5fded6d151ed796887f32
Robert Sapunarich <robertsapunarich@gmail.com>
2024-01-24 13:35:28 -0500

    Handle task status changes.

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -3,15 +3,81 @@ require "test_helper"
 class TasksControllerTest < ActionDispatch::IntegrationTest
 
   test "a project manager can mark a task as done" do
+    emp = employees(:project_manager)
+    task = tasks(:task)
+
+    params = {
+      status: "done"
+    }
+
+    patch "/tasks/#{task.id}/status", headers: {"Authorization": "Bearer #{emp.jwt}"}, params: params
+
+    assert_response :success
+    assert_equal("done", task.reload.status)
   end
 
   test "any other employee cannot mark a task as done" do
+    emp = employees(:developer)
+    task = tasks(:task)
+
+    params = {
+      status: "done"
+    }
+
+    patch "/tasks/#{task.id}/status", headers: {"Authorization": "Bearer #{emp.jwt}"}, params: params
+
+    assert_response :bad_request
+    assert_equal("not_started", task.reload.status)
   end
 
-  test "anyone can mark a task as not_started, working, or needs_review"
+  test "anyone can mark a task as not_started, working, or needs_review" do
+    proj_mgr = employees(:project_manager)
+    task = tasks(:task)
+
+    params = {
+      status: "working"
+    }
+
+    patch "/tasks/#{task.id}/status", headers: {"Authorization": "Bearer #{proj_mgr.jwt}"}, params: params
+
+    assert_response :success
+    assert_equal("working", task.reload.status)
+
+    dev = employees(:developer)
+
+    params = {
+      status: "needs_review"
+    }
+
+    patch "/tasks/#{task.id}/status", headers: {"Authorization": "Bearer #{dev.jwt}"}, params: params
+
+    assert_response :success
+    assert_equal("needs_review", task.reload.status)
   end
 
   test "no one can mark a task as late" do
+    proj_mgr = employees(:project_manager)
+    task = tasks(:task)
+
+    params = {
+      status: "late"
+    }
+
+    patch "/tasks/#{task.id}/status", headers: {"Authorization": "Bearer #{proj_mgr.jwt}"}, params: params
+
+    assert_response :bad_request
+    assert_equal("not_started", task.reload.status)
+
+    dev = employees(:developer)
+
+    params = {
+      status: "late"
+    }
+
+    patch "/tasks/#{task.id}/status", headers: {"Authorization": "Bearer #{dev.jwt}"}, params: params
+
+    assert_response :bad_request
+    assert_equal("not_started", task.reload.status)
   end
-  
+
 end
