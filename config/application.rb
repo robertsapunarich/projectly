7a3825b5ce379bdef2772d46eca2fdaf60d6744a
Robert Sapunarich <robertsapunarich@gmail.com>
2024-01-25 11:13:48 -0500

    empty commit to clean up application config.

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,8 @@ module Projectly
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-<<<<<<< HEAD
     config.middleware.use Rack::Cors
 
-=======
->>>>>>> prod
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
@@ -28,10 +25,8 @@ module Projectly
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-<<<<<<< HEAD
 
     config.jwt_secret_key = 'supersecretkey'
-=======
->>>>>>> prod
+  
   end
 end
